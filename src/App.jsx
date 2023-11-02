2edf7e43680e50edaa0cde28bbe2607de90036a4
Harshit Kumar <harshit892000@gmail.com>
2023-11-02 18:27:29 +0530

    Learn 2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import MyHeading from "./Components/MyHeading";
+const a="200fjvkz";
+
+
+function App()
+  {
+   
+  return <div>
+    <MyHeading/>
+    <h1>Welcome page 1 {a}</h1>
+  </div>
+ 
+  
+  }
+export default App;
---
+++ src/Components/MyHeading.jsx
@@ -0,0 +1,21 @@
+const MyHeading= ()=> 
+{
+const a=232;
+
+return(
+    <>
+
+    <h1>asdsas</h1>
+    <div>{a}</div>
+    <MyHeadingLittle/>
+    </>
+    
+    
+    
+);
+
+}
+
+const MyHeadingLittle=()=><h4>MyHeadingLittle</h4>
+
+export default MyHeading;
