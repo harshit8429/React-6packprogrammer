773421f9a3837d9f1405dafa5698338c8a34e5a9
Harshit Kumar <harshit892000@gmail.com>
2023-11-05 18:01:52 +0530

    learn 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Fragment } from "react";
+// import { Fragment } from "react";
 import MyHeading from "./Components/MyHeading";
-const a="200fjvkz";
+import Header from './Components/Header';
+// const a="200fjvkz";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
 
 
 function App()
   {
-   
-  return <div>
-    <MyHeading/>
-    <h1>Welcome page 1 {a}</h1>
-  </div>
+   <Header/>
+  return <Router>
+  <Routes>
+ <Route path="/" element={<Home/>}/>
+ <Route path="/about" element={<About/>}/>
+ <Route path="/contact" element={<Contact/>}/>
+
+  </Routes>
+  
+  </Router>
  
   
   }
